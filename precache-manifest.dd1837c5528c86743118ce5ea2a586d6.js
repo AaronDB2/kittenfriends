b57c8db735f57de0d4a60748e22dba3b45ad792c
Aaron de Boer <aarondeboerwk@gmail.com>
2020-10-08 17:20:38 +0200

    Updates

---
+++ precache-manifest.dd1837c5528c86743118ce5ea2a586d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca62e6389549116bd63a46d2f479781",
+    "url": "/kittenfriends/index.html"
+  },
+  {
+    "revision": "96a104b1baf7ec3ac9ef",
+    "url": "/kittenfriends/static/css/main.d4f00586.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/kittenfriends/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittenfriends/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a104b1baf7ec3ac9ef",
+    "url": "/kittenfriends/static/js/main.133a84a2.chunk.js"
+  },
+  {
+    "revision": "5a081eb9f1c9409b6329",
+    "url": "/kittenfriends/static/js/runtime-main.165f49d8.js"
+  }
+]);
